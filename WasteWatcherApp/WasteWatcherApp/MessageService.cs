5203b40e7c2d6585a34d21af598798b4ff10f826
MarioGeier00 <55717820+MarioGeier00@users.noreply.github.com>
2021-04-28 17:12:44 +0200

    MessageService refactoring

--- WasteWatcherApp/WasteWatcherApp/MessageService.cs
+++ WasteWatcherApp/WasteWatcherApp/MessageService.cs
@@ -7,24 +7,28 @@ namespace WasteWatcherApp
 {
     public static class MessageService
     {
-        private static IMessage _ToastService;
+        static IMessage _ToastService;
 
-        public static IMessage ToastService
+        static IMessage ToastService
         {
             get
             {
+                // Get IMessage implementation only once as soon as it is requested
+                // to improve performance and decrease memory usage
                 if (_ToastService == null)
                 {
                     _ToastService = DependencyService.Get<IMessage>();
                 }
-                
+
                 return _ToastService;
             }
         }
+
         public static void ShowToastShort(string message)
         {
             ToastService.ShortAlert(message);
         }
+
         public static void ShowToastLong(string message)
         {
             ToastService.LongAlert(message);
