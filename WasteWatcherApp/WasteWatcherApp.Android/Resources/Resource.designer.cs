8214f3652f732dae73c653310dcbc0808dfb5d38
MarioGeier00 <55717820+MarioGeier00@users.noreply.github.com>
2021-04-30 13:36:49 +0200

    Rename image

--- WasteWatcherApp/WasteWatcherApp.Android/Resources/Resource.designer.cs
+++ WasteWatcherApp/WasteWatcherApp.Android/Resources/Resource.designer.cs
@@ -19655,22 +19655,25 @@ namespace WasteWatcherApp.Droid
 			public const int notify_panel_notification_icon_bg = 2131165327;
 			
 			// aapt resource value: 0x7F070090
-			public const int Recycle = 2131165328;
+			public const int plastic_waste_on_beach_dark = 2131165328;
 			
 			// aapt resource value: 0x7F070091
-			public const int roundedbg = 2131165329;
+			public const int recycling = 2131165329;
 			
 			// aapt resource value: 0x7F070092
-			public const int roundedbgdark = 2131165330;
+			public const int roundedbg = 2131165330;
 			
 			// aapt resource value: 0x7F070093
-			public const int test_custom_background = 2131165331;
+			public const int roundedbgdark = 2131165331;
 			
 			// aapt resource value: 0x7F070094
-			public const int tooltip_frame_dark = 2131165332;
+			public const int test_custom_background = 2131165332;
 			
 			// aapt resource value: 0x7F070095
-			public const int tooltip_frame_light = 2131165333;
+			public const int tooltip_frame_dark = 2131165333;
+			
+			// aapt resource value: 0x7F070096
+			public const int tooltip_frame_light = 2131165334;
 			
 			static Drawable()
 			{
