1d809e8b540986dcd087f5680d7788f4315c050e
MarioGeier00 <55717820+MarioGeier00@users.noreply.github.com>
2021-05-02 11:24:17 +0200

    Add images to display eco-score from a to e

--- WasteWatcherApp/WasteWatcherApp/ProductInfo.xaml.cs
+++ WasteWatcherApp/WasteWatcherApp/ProductInfo.xaml.cs
@@ -17,31 +17,31 @@ namespace WasteWatcherApp
 
             Package.Text = product.Package;
             Brand.Text = product.Brand;
-            
+
+            bool hasBrand = product.Brand != null || product.Brand.Length > 0;
+            BrandContainer.IsVisible = hasBrand;
+            BrandUnavailableContainer.IsVisible = !hasBrand;
+
             switch (product.EcoScore)
             {
                 case "1":
-                    EcoImage.Source = "ecoscore_a.png";
-                    Eco_Score.Text = product.EcoScore;
+                    EcoImage.Source = "eco_score_a.png";
                     break;
                 case "2":
-                    EcoImage.Source = "Nutri_Score_B.png";
-                    Eco_Score.Text = product.EcoScore;
+                    EcoImage.Source = "eco_score_b.png";
                     break;
                 case "3":
-                    EcoImage.Source = "Nutri_Score_C.png";
-                    Eco_Score.Text = product.EcoScore;
+                    EcoImage.Source = "eco_score_c.png";
                     break;
                 case "4":
-                    EcoImage.Source = "Nutri_Score_D.png";
-                    Eco_Score.Text = product.EcoScore;
+                    EcoImage.Source = "eco_score_d.png";
                     break;
                 case "5":
-                    EcoImage.Source = "Nutri_Score_E.png";
-                    Eco_Score.Text = product.EcoScore;
+                    EcoImage.Source = "eco_score_e.png";
                     break;
                 default:
-                    Eco_Score.Text = "Kein Eco-Score angegeben!";
+                    EcoScoreContainer.IsVisible = false;
+                    EcoScoreUnavailableContainer.IsVisible = !EcoScoreContainer.IsVisible;
                     break;
             }
         }
