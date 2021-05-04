17fe3ba211abac7a12d4b8a1a275a5274933ee57
mario <marioegbertgeier@gmail.com>
2021-05-04 19:43:49 +0200

    Add waste store interface

---
+++ WasteWatcherApp/WasteWatcherApp/IWasteStore.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace WasteWatcherApp
+{
+    public interface IWasteStore
+    {
+        Task SaveData(string plasticWaste, string paperWaste, string glasWaste);
+    }
+}
