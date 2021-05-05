f5f2fb48d6f154de79092be19a6f55fdd9b12d09
mario <marioegbertgeier@gmail.com>
2021-05-05 11:28:00 +0200

    Add GetData interface function

--- WasteWatcherApp/WasteWatcherApp/IWasteStore.cs
+++ WasteWatcherApp/WasteWatcherApp/IWasteStore.cs
@@ -4,6 +4,7 @@ namespace WasteWatcherApp
 {
     public interface IWasteStore
     {
-        Task SaveData(string plasticWaste, string paperWaste, string glasWaste);
+        Task SaveData(string productId, string plasticWaste, string paperWaste, string glasWaste);
+        Task<WasteData> GetData(string productId);
     }
 }
