5f5866005fa1b6c55e71d4841f4901029f7d424f
mario <marioegbertgeier@gmail.com>
2021-05-25 09:16:14 +0200

    Add comment to WasteAmount class

--- WasteWatcherApp/WasteWatcherApp/Waste/WasteAmount.cs
+++ WasteWatcherApp/WasteWatcherApp/Waste/WasteAmount.cs
@@ -2,6 +2,10 @@
 {
     public record WasteAmount(WasteType WasteType, int Amount)
     {
+        /// <summary>
+        /// Creates a user friendly string of the current WasteAmount
+        /// </summary>
+        /// <returns>A string in the format [WasteTypeString]: [WasteAmount]g</returns>
         public override string ToString() => $"{WasteType.ToFriendlyName()}: {Amount}g";
     }
 }
