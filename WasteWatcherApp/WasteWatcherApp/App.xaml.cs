9138fd7cf32db25fcb3e5406593f66320ef22b0e
mario <marioegbertgeier@gmail.com>
2021-05-25 09:02:30 +0200

    Add app creation comments

--- WasteWatcherApp/WasteWatcherApp/App.xaml.cs
+++ WasteWatcherApp/WasteWatcherApp/App.xaml.cs
@@ -5,15 +5,24 @@ namespace WasteWatcherApp
 {
     public partial class App : Application
     {
+        /// <summary>
+        /// Initially called to create the application and the first page to display.
+        /// Creates a source chain <see cref="OpenFoodFactsProductSource"/> -> <see cref="ProductCache"/> -> <see cref="ProductJsonParser"/> -> <see cref="ProductRequestStore"/>.
+        /// </summary>
         public App()
         {
             InitializeComponent();
 
+            // Creates a new instance of the OpenFoodFactsApi and adds a ProductCache on top of that
             OpenFoodFactsProductSource apiSource = new();
             ProductCache productCacheWithApiSource = new(apiSource);
 
+            // Maps the json string from the OpenFoodFactsApi and the store to an
+            // instance of the class ProductData
             ProductJsonParser parsedProductSource = new(productCacheWithApiSource);
 
+            // The request store is used to save the requested barcode and time to the local storage
+            // to provide waste statistics like total waste consumption
             ProductRequestStore requestStore = new(parsedProductSource);
             MainPage = new NavigationPage(new MainPage(requestStore));
         }
