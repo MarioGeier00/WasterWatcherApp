2ad58e74945e7f76f8055771758dbca1af0e43f7
Majo <Majo@DESKTOP-0BQ7N6J>
2021-06-16 22:08:48 +0200

    Disable screen rotation and landscape mode

--- WasteWatcherApp/WasteWatcherApp.Android/MainActivity.cs
+++ WasteWatcherApp/WasteWatcherApp.Android/MainActivity.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
@@ -8,15 +6,17 @@ using Acr.UserDialogs;
 
 namespace WasteWatcherApp.Droid
 {
-    [Activity(Label = "WasteWatcherApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "WasteWatcherApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme",
+        MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+        ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            
+
             UserDialogs.Init(this);
-            
+
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             ZXing.Net.Mobile.Forms.Android.Platform.Init();
@@ -25,7 +25,7 @@ namespace WasteWatcherApp.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            
+
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
